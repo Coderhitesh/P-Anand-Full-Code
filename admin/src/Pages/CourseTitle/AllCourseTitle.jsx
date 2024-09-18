8f8aece8aba6189cae0c4adf1c8e9dd833a42efa
Hitesh Yadav <hiteshy468@gmail.com>
2024-09-18 12:24:47 +0530

    done

--- admin/src/Pages/CourseTitle/AllCourseTitle.jsx
+++ admin/src/Pages/CourseTitle/AllCourseTitle.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
 import Swal from 'sweetalert2';
-import { ToastContainer, toast } from 'react-toastify';
+// import { ToastContainer, toast } from 'react-toastify';
+import toast, { Toaster } from 'react-hot-toast';
 import 'react-toastify/dist/ReactToastify.css';
 
 function AllCourseTitle() {
@@ -68,7 +69,8 @@ function AllCourseTitle() {
 
     return (
         <>
-            <ToastContainer />
+            {/* <ToastContainer /> */}
+            <Toaster />
             <div className="bread">
                 <div className="head">
                     <h4>All Course Title </h4>
