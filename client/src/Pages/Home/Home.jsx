7e31d43e60fe3f16298c12f72c3f2ac64c77a2ce
Hitesh Yadav <hiteshy468@gmail.com>
2024-09-21 15:12:05 +0530

    remove team from home

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -55,7 +55,7 @@ function Home() {
             <OurClient />
             <FounderHome />
             <Features />
-            <OurTeam />
+            {/* <OurTeam /> */}
           </>
         )
       }
