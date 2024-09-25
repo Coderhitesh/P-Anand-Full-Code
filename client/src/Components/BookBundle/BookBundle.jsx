980534621c095fa5268fe55b031eed281258c42d
Coderhitesh <hiteshy468@gmail.com>
2024-09-25 16:16:27 +0530

    add

--- client/src/Components/BookBundle/BookBundle.jsx
+++ client/src/Components/BookBundle/BookBundle.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
