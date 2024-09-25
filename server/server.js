cf3bbc2fb34ba878afae48b05e03a0a14e506394
Coderhitesh <hiteshy468@gmail.com>
2024-09-25 15:30:01 +0530

    add

--- server/server.js
+++ server/server.js
@@ -29,12 +29,13 @@ const limiter = rateLimit({
 
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // All other routes should serve the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
+
 
 // app.use('./public/artits',express.static('files'))
 // app.use('/files',express.static(path.join(__dirname,'artits')))
